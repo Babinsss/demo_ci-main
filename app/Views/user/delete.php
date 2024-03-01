d4687d5b9ff352a9d0daaad478b3042738988fdf
Binns <vincentbarrientos51@gmail.com>
2024-03-01 11:59:16 +0800

    intl commit

--- app/Views/user/delete.php
+++ app/Views/user/delete.php
@@ -5,7 +5,7 @@
 <div class="container mt-5">
     <h3>Are you sure you want to delete this user?</h3>
 
-    <form action="<?= base_url("user/delete/{$user->user_id}") ?>" method="post">
+    <form id="deleteForm" action="<?= base_url("user/delete/{$user->user_id}") ?>" method="post">
         <?= csrf_field() ?>
 
         <div class="row mt-3">
@@ -32,10 +32,37 @@
         </div>
 
         <div class="mt-3">
-            <button type="submit" class="btn btn-danger" name="confirm_delete" value="1">Delete</button>
-            <a href="<?= base_url('/user/list') ?>" class="btn btn-primary">Cancel</a>
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmationModal">Delete</button>
+            <a href="<?= base_url('/') ?>" class="btn btn-primary">Cancel</a>
         </div>
     </form>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to delete this user?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="submit" class="btn btn-danger" form="deleteForm" name="confirm_delete" value="1">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?= $this->endSection('content') ?>
+
+<script>
+    // Optionally, you can include jQuery here if not already included
+    // Ensure that Bootstrap JS is also included in your project
+    // <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+</script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</script>
